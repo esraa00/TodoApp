aef134537866164baef20f4c0d73cad7793d3244
Esraa Abodeif <em4728644@gmail.com>
2022-06-27 15:15:47 +0200

    updated model validation

--- backend/models/task.js
+++ backend/models/task.js
@@ -4,11 +4,26 @@ const User = sequelize.define("Task", {
   taskName: {
     type: DataTypes.STRING,
     allowNull: false,
-    unique: true,
+    unique: {
+      args: true,
+      msg: "task name must be unique",
+    },
+    validate: {
+      notEmpty: {
+        args: true,
+        msg: "task name cannot be empty",
+      },
+    },
   },
   taskDescription: {
     type: DataTypes.STRING,
     allowNull: false,
+    validate: {
+      notEmpty: {
+        args: true,
+        msg: "task description cannot be empty",
+      },
+    },
   },
   isCompleted: {
     type: DataTypes.BOOLEAN,
