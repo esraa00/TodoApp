aa5a5e5edbb99cac45fe37957009cd17db6080e9
Esraa Abodeif <em4728644@gmail.com>
2022-06-27 15:37:06 +0200

    added middleware

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 
 const taskRoutes = require("./routes/task");
+const catchErrorsMiddleware = require("./middleware/catchErrors");
 
 const connectToDatabase = require("./database/connection");
 connectToDatabase();
@@ -10,6 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded());
 
 app.use("/api/v1/task", taskRoutes);
+app.use(catchErrorsMiddleware);
 
 app.listen(4000, () => {
   console.log("server is listening on port 4000");
