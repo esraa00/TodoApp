46f6a4306b88f252c6de11aea0831125fe0bef37
Esraa Abodeif <em4728644@gmail.com>
2022-06-28 17:30:16 +0200

    deleted old task controller

--- backend/controller/index.js
+++
@@ -1,6 +0,0 @@
-const { createTask } = require("./createTask");
-const { getTask } = require("./getTask");
-const { updateTask } = require("./updateTask");
-const { deleteTask } = require("./deleteTask");
-const { getAllTasks } = require("./getAllTasks");
-module.exports = { createTask, getTask, updateTask, deleteTask, getAllTasks };
