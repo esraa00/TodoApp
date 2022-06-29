2294a42072963f17288b1b8487c253239b83bd7a
Esraa Abodeif <em4728644@gmail.com>
2022-06-29 21:09:33 +0200

    created get user controller

---
+++ backend/controller/user/getUser.js
@@ -0,0 +1,16 @@
+const User = require("../../models/user");
+const getUser = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const userFound = await User.findOne({ where: { id } });
+    if (!userFound)
+      return res
+        .status(404)
+        .send({ status: "failure", response: "user not found" });
+    res.status(200).send({ statu: "success", response: userFound });
+  } catch (error) {
+    res.status(500).send({ status: "failure", respone: error });
+  }
+};
+
+module.exports = { getUser };
