2a8568f79d723476d6dc581da23c04e5b3d25157
Esraa Abodeif <em4728644@gmail.com>
2022-06-29 21:09:09 +0200

    created get all users controller

---
+++ backend/controller/user/getAllUsers.js
@@ -0,0 +1,11 @@
+const User = require("../../models/user");
+const getAllUsers = async (req, res) => {
+  try {
+    const usersFound = await User.findAll();
+    res.status(200).send({ status: "success", respone: usersFound });
+  } catch (error) {
+    res.status(500).send({ status: "failure", respone: error });
+  }
+};
+
+module.exports = { getAllUsers };
