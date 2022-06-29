f4b4a7ab36c3a0b36086c3669584dd23f3c7c468
Esraa Abodeif <em4728644@gmail.com>
2022-06-29 21:10:13 +0200

    created entry point for user controller

---
+++ backend/controller/user/index.js
@@ -0,0 +1,6 @@
+const { createUser } = require("./createUser");
+const { deleteUser } = require("./deleteUser");
+const { getUser } = require("./getUser");
+const { getAllUsers } = require("./getAllUsers");
+const { updateUser } = require("./updateUser");
+module.exports = { createUser, deleteUser, getUser, getAllUsers, updateUser };
