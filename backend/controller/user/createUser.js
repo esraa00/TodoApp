50183f845382c3c63565095f57d6e1f214013282
Esraa Abodeif <em4728644@gmail.com>
2022-06-29 21:07:55 +0200

    created create user controller

---
+++ backend/controller/user/createUser.js
@@ -0,0 +1,25 @@
+const User = require("../../models/user");
+const bcrypt = require("bcrypt");
+
+const createUser = async (req, res) => {
+  const { firstName, lastName, email, password } = req.body;
+
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const createdUser = await User.create({
+      firstName,
+      lastName,
+      email,
+      password: hashedPassword,
+    });
+
+    res
+      .status(200)
+      .send({ status: "success", response: "user created successfully" });
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
+
+module.exports = { createUser };
