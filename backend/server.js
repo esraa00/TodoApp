32ff6347741fff9dfebaa267d34b30f261a8bc7f
Esraa Abodeif <em4728644@gmail.com>
2022-06-29 21:12:28 +0200

    added middleware for user routes

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 
 const taskRoutes = require("./routes/task");
+const userRoutes = require("./routes/user");
 const catchErrorsMiddleware = require("./middleware/catchErrors");
 
 const connectToDatabase = require("./database/connection");
@@ -11,6 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded());
 
 app.use("/api/v1/task", taskRoutes);
+app.use("/api/v1/user", userRoutes);
 app.use(catchErrorsMiddleware);
 
 app.listen(4000, () => {
