01397223635ef2dbef2ce79ddb69dc3235e52c9e
Esraa Abodeif <em4728644@gmail.com>
2022-07-05 02:21:29 +0200

    modified the entry point for middleware folder

---
+++ backend/middleware/index.js
@@ -0,0 +1,3 @@
+const { catchErrors } = require("./catchErrors");
+const { checkIfUserIsAuthorized } = require("./checkIfUserIsAuthorized");
+module.exports = { catchErrors, checkIfUserIsAuthorized };
